2b1f8043c50eda88a77d7dbe789642b9802a60a8
aurghya <aurghya@yahoo.com>
2018-02-02 20:08:39 +0530

    Program to create image containing the original and its mirror image

---
+++ realmirror.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include<opencv2/core/core.hpp>
+using namespace std;
+using namespace cv;
+int main()
+{
+	Mat img=imread("pic.jpg",1);
+	int rows=img.rows;
+	int cols=img.cols;
+	Mat realmirror(rows,2*cols,CV_8UC3,Scalar(0,0,0));
+	for(int i=0;i<rows;i++)
+	{
+		for(int j=0;j<cols;j++)
+			realmirror.at<Vec3b>(i,j)=img.at<Vec3b>(i,j);
+	}
+	for(int i=0;i<rows;i++)
+	{
+		for(int j=cols;j<2*cols;j++)
+		{
+			realmirror.at<Vec3b>(i,j)=img.at<Vec3b>(i,2*cols-j);
+		}
+	}
+	namedWindow("real mirror",WINDOW_NORMAL);
+	imshow("real mirror",realmirror);
+	imwrite("realmirror.jpg",realmirror);
+	waitKey(0);
+}
